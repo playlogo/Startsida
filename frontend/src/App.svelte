dd0d9404a9760fe43dbbda3080769210a8931d10
playlogo <playlogo@yahoo.com>
2024-12-31 16:08:49 +0100

    test

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -10,7 +10,7 @@
 	const chat = {
 		image: "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/4b/36/3d/4b363d2b-753f-f33c-8d6f-6475855d5404/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/230x0w.webp",
 		link: "http://192.168.178.64:3000/?orgId=1",
-		name: "Grafana",
+		name: "ChatGPT",
 	};
 </script>
 
